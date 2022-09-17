dca692bc4402d66eb1f46dcbacee27dfc755ecfe
Shimil Tharayil <shimiltharayil007@gmail.com>
2022-09-17 12:48:33 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './Css/App.css';
+
+function App() {
+  return (
+    <div className="app">
+            {/* Header */}
+
+
+              {/* TinderCards */}
+
+             {/* Swipes */}
+
+    </div>
+  );
+}
+
+export default App;
